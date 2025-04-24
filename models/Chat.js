32ff71aa8f52fcc246a7bb14163320ef57d2ccff
helloredreyes <hello.silentlymine@gmail.com>
2025-04-24 16:57:37 +0700

    adjust mongodb

--- models/Chat.js
+++ models/Chat.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const chatSchema = new mongoose.Schema({
     username: { type: String, required: true },
     message: { type: String, required: true },
-    timestamp: { type: String, required: true }, // Change from Date to String
-});
+    timestamp: { type: String, required: true },
+}, { collection: 'chats' }); // Explicitly set the collection name
 
 module.exports = mongoose.model('Chat', chatSchema);
