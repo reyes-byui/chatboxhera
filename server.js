83a3b0f4f266a9281496ed83808b96161f2c63ca
helloredreyes <hello.silentlymine@gmail.com>
2025-04-24 17:04:20 +0700

    debug

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const io = new Server(server, {
     },
 });
 
+mongoose.set('debug', true); // Enable Mongoose debugging
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
